9d90c2fee826731fbfeb5c432c4ad10056e0c8fe
Yujian <yujian_fu@outlook.com>
2020-11-13 07:07:33 +0800

    test DBA

--- config.py
+++ config.py
@@ -1,7 +1,5 @@
-import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "2"
 import torch
-device=torch.device('cuda:1' if torch.cuda.is_available() else 'cpu')
+device=torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 AGGR_MEAN = 'mean'
 AGGR_GEO_MED = 'geom_median'
